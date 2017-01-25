5d68d1e9ed5feffa6fc1aabb276febfe745a139e
Stefan Kowal <kowalmax.s@gmail.com>
2017-01-25 14:03:48 -0700

    adding content from routing tutorial

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,50 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [ 
   	BrowserModule,
-  	FormsModule
+  	FormsModule,
+  	RouterModule.forRoot([
+	  {
+	    path: 'heroes',
+	    component: HeroesComponent
+	  },
+	  {
+	  	path: 'dashboard',
+	  	component: DashboardComponent
+	  },
+	  {
+	  	path: '',
+	  	redirectTo: '/dashboard',
+	  	pathMatch: 'full'
+	  },
+	  {
+	  	path: 'details/:id',
+	  	component: HeroDetailComponent
+	  }
+	])
   ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ 
+  	AppComponent,
+  	HeroDetailComponent,
+  	HeroesComponent,
+  	DashboardComponent
+  	],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 
 export class AppModule { }
+
+
